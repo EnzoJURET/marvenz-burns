ba616d70b1328dfd7c7afdd138170b88ff696661
Marvin COUSIN <marvin.cousin05@gmail.com>
2020-04-24 18:32:08 +0200

    delete vich bundle

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -4,14 +4,12 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * Image
  *
  * @ORM\Table(name="image", indexes={@ORM\Index(name="image_slide_FK", columns={"id_slide"})})
  * @ORM\Entity
- * @Vich\Uploadable
  */
 class Image
 {
